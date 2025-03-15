e6c656208f202205c07212651c33543e17b56ff0
Michael Davis <mdavis332@gmail.com>
2025-03-15 11:43:07 -0500

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -9,7 +9,7 @@ brewInstall () {
     # Install the correct homebrew for each OS type
         if test "$(uname)" = "Darwin"
         then
-            ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+            "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
             echo 'eval $(/opt/homebrew/bin/brew shellenv)' >> $HOME/.zprofile
             eval "$(/opt/homebrew/bin/brew shellenv)"
             success 'brew installed'
